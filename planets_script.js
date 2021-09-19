d67c4657724b75a34c3b5d5b88e43837fddbf8c0
Tamara Wenner <tamara-76@hotmail.fr>
2021-09-19 19:28:40 +0200

    Ajout des light_button, changement de couleur des planètes, fonction changement de planète modifiée

--- planets_script.js
+++ planets_script.js
@@ -5,12 +5,17 @@ const planet_diameter = document.getElementById("planet_diameter");
 const planet_orbital_p= document.getElementById("planet_orbital_p");
 const planet_rotation_p= document.getElementById("planet_rotation_p");
 const planet_population= document.getElementById("planet_pop");
-
+const planet_distance = document.getElementById("planet_distance");
+const planet_globe = document.getElementById("planet_globe");
 const arrows = document.getElementsByClassName("slick-arrow");
+
+// Get the root element
+var r = document.querySelector(':root');
 
 let planet_id = 1;
 
-function getPlanet(id){
+async function getPlanet(id){
+  //console.log("getplanet "+Date.now());
     const url = 'https://swapi.dev/api/planets/'+id;
     const options={
         method : 'GET', 
@@ -27,37 +32,46 @@ function getPlanet(id){
         planet_orbital_p.textContent=JSON.stringify(data.orbital_period).replaceAll('"','');
         planet_rotation_p.textContent=JSON.stringify(data.rotation_period).replaceAll('"','');
         planet_population.textContent=JSON.stringify(data.population).replaceAll('"','');
+        planet_distance.textContent=randomLightYears();
+        
     })   
     .catch(err =>console.log("Il y a erreur", err))
     .catch(err =>console.log("Il y a erreur json", err))
+    return await Promise.resolve();
    }
 
 
 
-function randomPlanet(){
-    // Returns a random integer from 1 to 60:
-   return Math.floor(Math.random() * 60) + 1;
+function randomLightYears(){
+    // Returns a random integer from 1 to 5000
+   return Math.floor(Math.random() * 5000) + 1;
 }
 
 
 //PREVIOUS ARROW
 arrows[0].addEventListener("click", function(){ 
+  
   if(planet_id==1){
     planet_id=60;
   }
   else{
     planet_id-=1;
   }
-  planet_container.classList.add('container_animation');
-  setTimeout(function(){
-    getPlanet(planet_id);
-  }, 750);
-  setTimeout(function(){
-    planet_container.classList.remove('container_animation');
-  }, 1500);
+  planet_container.classList.add('disappear');
+  console.log("add disappear"+Date.now());
+  getPlanet(planet_id).then( (response) => {
+      //console.log("getplanet "+Date.now());
+      changePlanetColor(planet_id);
+      planet_container.classList.remove('disappear');
+      planet_container.classList.add('appear');
+      setTimeout(function(){
+        //console.log("removing appear in 1.2s  " +Date.now());
+        planet_container.classList.remove('appear');
+        }, 1500); //Turning off button until animation is over ???
+    });
 });
 
-//NEXT
+//NEXT ARROW
 arrows[1].addEventListener("click", function(){ 
   if(planet_id==60){
     planet_id=1;
@@ -65,34 +79,90 @@ arrows[1].addEventListener("click", function(){
   else{
     planet_id+=1;
   }
-  planet_container.classList.add('container_animation');
-  setTimeout(function(){
-    getPlanet(planet_id);
-  }, 750);
-  setTimeout(function(){
-    planet_container.classList.remove('container_animation');
-  }, 1500);
- });
+  planet_container.classList.add('disappear');
+  //console.log("add disappear"+Date.now());
+  getPlanet(planet_id).then( (response) => {
+      changePlanetColor(planet_id);
+      planet_container.classList.remove('disappear');
+      planet_container.classList.add('appear');
+      setTimeout(function(){
+        //console.log("removing appear in 1.2s  " +Date.now());
+        planet_container.classList.remove('appear');
+        }, 1500); //Turning off next button until animation is over ???
+    });
+      
+  });
+
 
 getPlanet(planet_id);
 
-function getPlanetImage(){
-  // const url = 'https://starwars.fandom.com/wiki/Tatooine?file=Tatooine_TPM.png';
-  // const options={
-  //     method : 'GET', 
-  //     headers : {
-  //       Accept: 'application/json', 
-  //       'Content-type' : 'application/json'}
-  //   }
-  fetch('https://starwars.fandom.com/wiki/Tatooine?file=Tatooine_TPM.png')
-   .then(reponse => {
+function changePlanetColor(){
+        switch(planet_id){
+          case 1: //TATOOINE
+              r.style.setProperty('--first_globe_color', '#bd592b');
+              r.style.setProperty('--second_globe_color', '#b89455');
+              r.style.setProperty('--third_globe_color', '#db821b');
+            break;
+          case 2: //ALDERAAN
+              r.style.setProperty('--first_globe_color', '#3ec462');
+              r.style.setProperty('--second_globe_color', '#30abff');
+              r.style.setProperty('--third_globe_color', '#ffffff');
+          break;
+          case 3: //YAVIN IV
+              r.style.setProperty('--first_globe_color', '#3ec462');
+              r.style.setProperty('--second_globe_color', '#30abff');
+              r.style.setProperty('--third_globe_color', '#ffffff');
+          break;
+          case 4: // HOTH
+              r.style.setProperty('--first_globe_color', '#00ffeb');
+              r.style.setProperty('--second_globe_color', '#30d8ff');
+              r.style.setProperty('--third_globe_color', '#ffffff');
+          break;
+          case 5: // DAGOBAH
+              r.style.setProperty('--first_globe_color', '#a9c4a3');
+              r.style.setProperty('--second_globe_color', '#188409');
+              r.style.setProperty('--third_globe_color', '#f5d3d3');
+          break;
+          case 6: // BESPIN
+              r.style.setProperty('--first_globe_color', '#debb48');
+              r.style.setProperty('--second_globe_color', '#ffed1e');
+              r.style.setProperty('--third_globe_color', '#a3691d');
+          break;
+          case 7: // ENDOR
+              r.style.setProperty('--first_globe_color', '#a9c4a3');
+              r.style.setProperty('--second_globe_color', '#188409');
+              r.style.setProperty('--third_globe_color', '#f5d3d3');
+              break;
+          case 8: // NABOO
+              r.style.setProperty('--first_globe_color', '#3ec462');
+              r.style.setProperty('--second_globe_color', '#30abff');
+              r.style.setProperty('--third_globe_color', '#ffffff');
+          break;
+          case 9: // CORUSCANT
+              r.style.setProperty('--first_globe_color', '#2c240e');
+              r.style.setProperty('--second_globe_color', '#494232');
+              r.style.setProperty('--third_globe_color', '#9e6922');
+          break;
+          case 10: // KAMINO
+              r.style.setProperty('--first_globe_color', '#0630d0');
+              r.style.setProperty('--second_globe_color', '#1881d4');
+              r.style.setProperty('--third_globe_color', '#0630d0');
+          break;
+          default:
+            r.style.setProperty('--first_globe_color', randomColor());
+            r.style.setProperty('--second_globe_color', randomColor());
+            r.style.setProperty('--third_globe_color', randomColor());
+          break;
+        }
+      }
 
-    var img = document.createElement('img');
-    img.src = reponse;
-    document.getElementById('body').appendChild(img);
 
-    })   
-  .catch(err =>console.log("Il y a erreur", err))
- }
-
-getPlanetImage();
+const randomColor = () => {
+  let color = '#';
+  for (let i = 0; i < 6; i++){
+     const random = Math.random();
+     const bit = (random * 16) | 0;
+     color += (bit).toString(16);
+  };
+  return color;
+};
